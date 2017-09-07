2ebba56426dd253d6bea6be89e2dedf2599a3ec5
johngtx <john_gtx@hotmail.com>
2017-09-07 17:38:59 +0800

    update

--- proxy/fscdr_api.js
+++ proxy/fscdr_api.js
@@ -12,21 +12,27 @@ module.exports = {
 }
 
 function translate_origin_cdr_data(data) {
-    return {
-        core_uuid: data["core-uuid"],
-        switchanme: data["switchname"],
-        caller_id_name: data["callflow"][0]["caller_profile"]["caller_id_name"],
-        caller_id_number: data["callflow"][0]["caller_profile"]["caller_id_number"],
-        destination_number: data["callflow"][0]["caller_profile"]["destination_number"],
-        context: data["callflow"][0]["caller_profile"]["conext"],
-        start_stamp: data["variables"]["start_stamp"],
-        answer_stamp: data["variables"]["answer_stamp"],
-        end_stamp: data["variables"]["end_stamp"],
-        duration: data["variables"]["duration"],
-        billsec: data["variables"]["billsec"],
-        hangup_cause: data["variables"]["hangup_cause"],
-        uuid: data["variables"]["uuid"],
-        bleg_uuid: '',
-        account_code: data["variables"]["accountcode"]
+    try {
+        let obj = JSON.parse(data);
+        return {
+            core_uuid: obj["core-uuid"],
+            switchanme: obj["switchname"],
+            caller_id_name: obj["callflow"][0]["caller_profile"]["caller_id_name"],
+            caller_id_number: obj["callflow"][0]["caller_profile"]["caller_id_number"],
+            destination_number: obj["callflow"][0]["caller_profile"]["destination_number"],
+            context: obj["callflow"][0]["caller_profile"]["conext"],
+            start_stamp: obj["variables"]["start_stamp"],
+            answer_stamp: obj["variables"]["answer_stamp"],
+            end_stamp: obj["variables"]["end_stamp"],
+            duration: obj["variables"]["duration"],
+            billsec: obj["variables"]["billsec"],
+            hangup_cause: obj["variables"]["hangup_cause"],
+            uuid: obj["variables"]["uuid"],
+            bleg_uuid: '',
+            account_code: obj["variables"]["accountcode"]
+        }
+    } catch (e) {
+        console.log(e);
+        return {};
     }
 }
